494ed09d0e39778e24291ab3217a4de01704486f
joo <joo@>
2018-10-11 23:21:21 +0900

    test1

--- pdfbox_sample/src/test/java/pdfbox_sample/pdfboxTest.java
+++ pdfbox_sample/src/test/java/pdfbox_sample/pdfboxTest.java
@@ -6,10 +6,10 @@ public class pdfboxTest {
 
 	@Test
 	public void testfuc(){
-		String s ="somansa zzang zzang";
-		assertTrue(s.equals("somansa zzang zzang"));	
+		String s ="somansa zzang zzang1";
+		assertTrue(s.equals("somansa zzang zzang"));
 	}
-	
+
 	@Test
 	public void testfuc2(){
 		String s ="somansa zzang zzang";
