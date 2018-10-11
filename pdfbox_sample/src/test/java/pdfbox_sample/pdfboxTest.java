f562e95e5ecee79a7bebf1400f2ee3feafdf131b
joo <wntjq01@naver.com>
2018-10-11 22:55:52 +0900

    junit 추가 및 테스트 코드 추가.

---
+++ pdfbox_sample/src/test/java/pdfbox_sample/pdfboxTest.java
@@ -0,0 +1,18 @@
+package pdfbox_sample;
+import static org.junit.Assert.*;
+
+import org.junit.*;
+public class pdfboxTest {
+
+	@Test
+	public void testfuc(){
+		String s ="somansa zzang zzang";
+		assertTrue(s.equals("somansa zzang zzang"));	
+	}
+	
+	@Test
+	public void testfuc2(){
+		String s ="somansa zzang zzang";
+		assertTrue(s.equals("somansa zzang zzang man"));
+	}
+}
